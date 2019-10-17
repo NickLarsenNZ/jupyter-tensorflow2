65d7720e763379f6d912bbe75bf2a6415d2c87c8
Nick <NickLarsenNZ@users.noreply.github.com>
2019-10-17 14:18:59 +1300

    Corrected docker image name

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 PORT=${PORT:-8999}
 
-docker run --rm --name jupyter -v $HOME/Development/notebooks:/data:rw -p ${PORT}:8080 -e EXTERNAL_PORT=${PORT} nicklarsennz/jupyter-tensorflow:latest
+docker run --rm --name jupyter -v $HOME/Development/notebooks:/data:rw -p ${PORT}:8080 -e EXTERNAL_PORT=${PORT} nicklarsennz/jupyter-tensorflow2:latest
